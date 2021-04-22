e999c5504ac39b347dd0cec3afb7418706602a21
Jia-Xiang Cheng <JIAXIANG002@E.NTU.EDU.SG>
2021-04-22 10:20:38 +0800

    First version finalized

--- visual_compare.m
+++ visual_compare.m
@@ -16,3 +16,17 @@ hold on
 plot(resultsindiv.naivel, resultsindiv.id, ':')
 hold on
 plot(resultsindiv.naiveu, resultsindiv.id, ':')
+
+
+%%
+
+figure
+for i = 2:101
+    plot(resultsindivS3(1:10,1),resultsindivS3(1:10,i),':','Color',[0,0.1,0.1])
+    hold on
+end
+hold on
+plot(resultsindivS3(1:10,1),resultsindivS3(1:10,102),'-','Color',[0,1,0.9])
+str = [repmat('',10,1) num2str(resultsindivS3(1:10,102))];
+hold on
+text(resultsindivS3(1:10,1),resultsindivS3(1:10,102),cellstr(str))
